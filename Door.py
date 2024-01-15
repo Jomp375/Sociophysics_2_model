18dec6814d8ccece42ad42e6a42985aec5bb37ae
Jomp375 <91837656+Jomp375@users.noreply.github.com>
2024-01-15 11:50:03 +0100

    number tweaking :)

--- Door.py
+++ Door.py
@@ -3,16 +3,16 @@ import numpy as np
 
 
 class Door:
-    def __init__(self, time_X, position_at_time_X, final_position, final_time, door_width):
+    def __init__(self, time_x, position_at_time_x, final_position, final_time, door_width):
         self.final_position = np.array(final_position)
         self.final_time = final_time
         self.door_width = door_width
 
         # Calculate initial velocity to ensure the door is standing still at the final time and position
-        self.initial_velocity = np.divide((final_position - position_at_time_X), (final_time - time_X))
+        self.initial_velocity = np.divide((final_position - position_at_time_x), (final_time - time_x))
 
         # Calculate initial position
-        self.initial_position = position_at_time_X - time_X * self.initial_velocity
+        self.initial_position = position_at_time_x - time_x * self.initial_velocity
 
         self.position = np.array(self.initial_position)
         self.velocity = np.array(self.initial_velocity)
